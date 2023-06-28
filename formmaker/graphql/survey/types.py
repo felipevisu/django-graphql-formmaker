70a683f51ff62e45a58f01a6e7fa8a4342a1d0ef
Felipe Faria <felipevisu@gmail.com>
2023-06-27 21:51:30 -0300

    fixup! feat: create Value model to add options to the Questions

--- formmaker/graphql/survey/types.py
+++ formmaker/graphql/survey/types.py
@@ -1,3 +1,4 @@
+import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 
@@ -10,7 +11,19 @@ class Survey(DjangoObjectType):
         interfaces = (relay.Node,)
 
 
+class QuestionValue(DjangoObjectType):
+    class Meta:
+        model = models.QuestionValue
+        interfaces = (relay.Node,)
+
+
 class Question(DjangoObjectType):
+    values = graphene.List(QuestionValue)
+
     class Meta:
         model = models.Question
         interfaces = (relay.Node,)
+
+    @staticmethod
+    def resolve_values(question, *args, **kwargs):
+        return question.values.all()
